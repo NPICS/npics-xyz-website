8641d2da9c367a83d50e0e22e8b7a82214506785
rainsun3201 <rainsun3201@protonmail.com>
2022-06-21 11:40:44 +0800

    Supplement NFT external link

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -8,4 +8,18 @@ export const urls = {
     contactUs: "https://docs.google.com/forms/d/e/1FAIpQLSdZVptvuauolOiMSIYb5F3wn2foiKnm7Z0IclOHxvObY_lJrw/viewform?usp=pp_url",
     Resources: "https://npics.gitbook.io/npics-v1.0/about/introduce-npics",
     termsOfService: "https://npics.gitbook.io/npics-v1.0/resources/terms-of-service",
+
+    etherscanTxDetail(tx: string) {
+      return `https://etherscan.io/tx/${tx}`
+    },
+
+    etherscanNft(nftAddress: string, tokenId: number | string) {
+      return `https://etherscan.io/nft/${nftAddress}/${tokenId}`
+    },
+
+    raritysniper(name: string, tokenId: number | string) {
+      // example: https://raritysniper.com/bored-ape-yacht-club/2668
+      // name need convert
+      return `https://raritysniper.com/${name}/${tokenId}`
+    }
 }
