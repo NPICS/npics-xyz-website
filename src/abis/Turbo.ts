b512718049b2612a3c3fe5f3a0e3de031df019fc
AntPong <hello@npics.xyz>
2022-08-09 18:30:39 +0800

    change Trubo NFT network

--- src/abis/Turbo.ts
+++ src/abis/Turbo.ts
@@ -9,8 +9,8 @@ export class Turbo {
   }
 
   async allowlistMint() {
-    // const TurboContract = new ethers.Contract(ContractAddresses.TurboProxy, TurboABI, this.signer);
-    const TurboContract = new ethers.Contract(ContractAddresses.TurboTestProxy, TurboABI, this.signer);
+    const TurboContract = new ethers.Contract(ContractAddresses.TurboProxy, TurboABI, this.signer);
+    // const TurboContract = new ethers.Contract(ContractAddresses.TurboTestProxy, TurboABI, this.signer);
     // const TurboContract = new ethers.Contract("0xf15304428ab7d85938baa103fb82e90eed887315", TurboABI, this.signer);
     return await TurboContract.allowlistMint({ value: 0 });
 
