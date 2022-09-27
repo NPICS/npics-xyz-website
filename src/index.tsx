bf74a31fcfc88b7b1ac2fbb4b8022fb77123fa1a
Antpong <hello@npics.xyz>
2022-09-27 11:16:02 +0800

    fix header

--- src/index.tsx
+++ src/index.tsx
@@ -60,7 +60,7 @@ const StyledThemeProvider = (props: any) => {
         action(setShowDetailAnimate(true))
       }
     }
-    scrollTop > 150 ? action(setFixed(true)) : action(setFixed(false))
+    scrollTop > 30 ? action(setFixed(true)) : action(setFixed(false))
   }
   const isDark = isHome
   return <ThemeProvider theme={isDark ? darkColors : lightColors} {...props} />
